1a73668a3f13cd226604270f4bf0d3c5a097ba3f
cirorey20 <cirorey95@gmail.com>
2022-08-02 16:34:05 -0300

    added model product

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,9 +2,11 @@ import { Sequelize } from "sequelize/types";
 
 // const { User, UserSchema } = require('./user.model.js');
 import { User, UserSchema } from "./models/user.model";
+import { Product, ProductSchema } from "./models/product.model";
 
 function setupModels(sequelize: Sequelize): void {
   User.init(UserSchema, User.config(sequelize));
+  Product.init(ProductSchema, Product.config(sequelize));
 }
 
 export default setupModels;
