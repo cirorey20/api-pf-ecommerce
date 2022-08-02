011bf96d6670bd68193362846853eed05c0dd480
KuraiRantan <jorgecamargo2012902@gmail.com>
2022-08-02 09:27:39 -0500

    add new coment

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -6,7 +6,7 @@ import postsRouter from './users/user.router';
 
 function routerApi(app: Express):void {
     const router:Router = express.Router();
-    app.use('/api/v1', router);//ruta de api principal
+    app.use('/api/v1', router);//ruta de api principal api
     router.use('/users', postsRouter);
 }
 
