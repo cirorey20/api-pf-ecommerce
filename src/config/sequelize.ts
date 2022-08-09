c29b80b498dfb7b6e0f33bfbbdfd5aa8bd54216b
cirorey20 <cirorey95@gmail.com>
2022-08-09 17:54:04 -0300

    add DATABASE_URL

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,4 +1,6 @@
 // const {Sequelize} = require('sequelize');
+import dotenv from 'dotenv';
+dotenv.config();
 import { Sequelize } from "sequelize";
 
 // const setupModels = require('../db/models/index');
@@ -7,9 +9,10 @@ import setupModels from "../db";
 // const {config} = require('../config/config');
 import { config } from "./config";
 
-const dbUrl: string = `postgres://${config.dbUser}:${config.dbPassword}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const dbUrl: string = `postgres://${config.dbUser}:${config.dbPassword}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const url: any = process.env.DATABASE_URL
 
-const sequelize: Sequelize = new Sequelize(dbUrl, {
+const sequelize: Sequelize = new Sequelize(url, {
   dialect: "postgres",
   logging: true,
 });
