86e070eb56f92a51cb3afb4ed84e191c3b4426d5
cirorey20 <cirorey95@gmail.com>
2022-08-11 13:24:00 -0300

    feat(src/index.js): add url front production

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 //aca vamos a poner los cors
 
 app.use((req: Request, res: Response, next: NextFunction): void => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://universalmusic-henry.web.app"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "*");
   res.header(
