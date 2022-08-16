972ef038f6a6daf03fe62f68b3737e560ae7c45e
cirorey20 <cirorey95@gmail.com>
2022-08-15 21:45:31 -0300

    index add url localhost comment

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,8 @@ app.use(express.json());
 //aca vamos a poner los cors
 
 app.use((req: Request, res: Response, next: NextFunction): void => {
-  res.header("Access-Control-Allow-Origin", "https://universalmusic-henry.web.app"); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://universalmusic-henry.web.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "*");
   res.header(
