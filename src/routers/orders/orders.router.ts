605c5fce16267dd82cda83cc96c6e15d1e001556
KuraiRantan <jorgecamargo2012902@gmail.com>
2022-08-16 14:49:55 -0500

    Fix delete router send mail

--- src/routers/orders/orders.router.ts
+++ src/routers/orders/orders.router.ts
@@ -1,12 +1,11 @@
 import express, { Router } from "express";
-import { checkout, getOrderById, getOrders, sendMail } from "../../controller/order.controller";
+import { checkout, getOrderById, getOrders } from "../../controller/order.controller";
 
 const router:Router = express.Router();
 
 router.get('/', getOrders);
 router.get('/:idOrder', getOrderById);
 router.post('/checkout', checkout);
-router.post('/send', sendMail)
 
 
 export default router;
