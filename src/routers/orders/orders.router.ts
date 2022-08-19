0fb8af24c7c95be731b561e938ab6b6354c614d8
Carlos Mendez <c.rmendez@gmail.com>
2022-08-19 17:23:02 -0500

    get orders by user done

--- src/routers/orders/orders.router.ts
+++ src/routers/orders/orders.router.ts
@@ -1,12 +1,16 @@
 import express, { Router } from "express";
-import { checkout, getOrderById, getOrders } from "../../controller/order.controller";
+import {
+  checkout,
+  getOrderById,
+  getOrders,
+  getOrdersByUser,
+} from "../../controller/order.controller";
 
-const router:Router = express.Router();
+const router: Router = express.Router();
 
-router.get('/', getOrders);
-router.get('/:idOrder', getOrderById);
-router.post('/checkout', checkout);
-
+router.get("/", getOrders);
+router.get("/:idOrder", getOrderById);
+router.get("/user/:idUser", getOrdersByUser);
+router.post("/checkout", checkout);
 
 export default router;
-
